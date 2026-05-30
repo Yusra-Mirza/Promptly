a2ce7c6839a707769f505a4a7f5bfd192d5d608d
Yusra <yusramirza555@gmail.com>
2026-05-30 18:02:19 +0530

    Set up Schemas and Models for User,Chat and Message

---
+++ BackEnd/Models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    name: String,
+    required: true,
+  },
+  pic: {
+    type: String,
+    required: true,
+    default: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+  },
+
+},
+{
+    timestamps:true,
+}
+);
+
+const User=mongoose.model("User",userSchema);
