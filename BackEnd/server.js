39d75b07a26fe5cedbede283c11d8a378295cc45
Yusra <yusramirza555@gmail.com>
2026-06-05 20:56:37 +0530

    Connected to Database

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,22 +1,9 @@
-
-import chats from "./src/data/data.js";
+import mongoose from "mongoose";
+// import chats from "./src/data/data.js";
 import config from "./src/config/config.js";
 import app from "./src/app.js";
-
-// app.get("/", (req, res) => {
-//   res.send("API is runnning");
-// });
-
-// app.get("/api/chat", (req, res) => {
-//   res.send(chats);
-// });
-
-// app.get("/api/chat/:id", (req, res) => {
-//   let { id } = req.params;
-//   const singleChat = chats.find((c) => c._id === id);
-//   // const singleChat=chats.findById(_id);
-//   res.send(singleChat);
-// });
+import connectDB from "./src/config/database.js";
+await connectDB();
 const port=config.PORT;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- BackEnd/src/config/config.js
+++ BackEnd/src/config/config.js
@@ -2,5 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 const config={
     PORT:process.env.PORT||8000,
+    MONGO_URI:process.env.MONGO_URI,
 }
 export default config;
