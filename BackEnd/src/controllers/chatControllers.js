e1d50436b011a1a22c5c1f0bb6f7a5b4ccf50dd4
Yusra <yusramirza555@gmail.com>
2026-06-11 23:27:07 +0530

    Create Group Creation API

--- BackEnd/src/controllers/chatControllers.js
+++ BackEnd/src/controllers/chatControllers.js
@@ -56,13 +56,41 @@ export const fetchChats = asyncHandler(async (req, res) => {
       .populate("groupAdmin", "-password")
       .populate("latestMessage")
       .sort({ updatedAt: -1 });
-    results=await User.populate(results,{
-        path:"latestMessage.sender",
-        select:"name pic email"
+    results = await User.populate(results, {
+      path: "latestMessage.sender",
+      select: "name pic email",
     });
     res.status(200).send(results);
   } catch (err) {
     res.status(400);
     throw new Error(err.message);
   }
+});
+export const createGroup = asyncHandler(async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    res.status(400).send({ message: "Please fill all the fields" });
+  }
+  let users=JSON.parse(req.body.users);
+  if(users.length<2){
+    return res.status(400).send("More than 2 users are required to form a group");
+  }
+  users.push(req.user._id);
+  try{
+    const groupChat=await Chat.create({
+      chatName:req.body.name,
+      users:users,
+      isGroupChat:true,
+      groupAdmin:req.user._id
+    });
+
+    const fullGroupChat=await Chat.findOne({_id:groupChat._id}).populate("users","-password").populate("groupAdmin","-password");
+      
+    res.status(201).json(fullGroupChat);
+
+    }catch(err){
+      res.status(400);
+      throw new Error(err.message);
+    }
+
+
 });
--- BackEnd/src/routes/chatRoutes.js
+++ BackEnd/src/routes/chatRoutes.js
@@ -1,12 +1,10 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 const router=express.Router();
-import { accessChat,fetchChats
-    
- } from "../controllers/chatControllers.js";
+import { accessChat,fetchChats,createGroup} from "../controllers/chatControllers.js";
 router.post("/",protect,accessChat);
 router.get("/",protect,fetchChats);
-// router.post("/group",protect,createGroupChat);
+router.post("/group",protect,createGroup);
 // router.put("/rename", protect, renameGroupChat);
 // router.put("/groupremove",protect,removeFromGroup);
 // router.put("/groupadd",protect,addToGroup); 
